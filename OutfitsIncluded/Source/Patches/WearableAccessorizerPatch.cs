e45635ad033f00b54ec20d2f30fd3f7ceca5b02d
Saffron <191891154+saffron-ink@users.noreply.github.com>
2025-03-11 07:58:27 -0700

    Default belt always visible unless overriden

---
+++ OutfitsIncluded/Source/Patches/WearableAccessorizerPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using SaffronLib;
+using System;
+
+namespace OutfitsIncluded.Patches
+{
+	public class WearableAccessorizerPatch
+	{
+		[HarmonyPatch(typeof(WearableAccessorizer))]
+		[HarmonyPatch(nameof(WearableAccessorizer.UpdateVisibleSymbols))]
+		[HarmonyPatch(new Type[] { typeof(WearableAccessorizer.WearableType) })]
+		public class WearableAccessorizer_UpdateVisibleSymbols_Patch
+		{
+			public static void Postfix(WearableAccessorizer __instance)
+			{
+				Log.Info("WearableAccessorizer_UpdateVisibleSymbols_Patch.Postfix()");
+				KAnimControllerBase animController = (KAnimControllerBase)Traverse.Create(__instance).Field("animController").GetValue();
+				animController.SetSymbolVisiblity(Db.Get().AccessorySlots.Belt.targetSymbolId, true);
+			}
+		}
+	}
+}
